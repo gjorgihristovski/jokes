66280526db8f254fa254c54a6117c3695e4c63d1
Gjorgi Hristovski <gjorgi.hristovski@mail.com>
2018-10-31 11:04:40 +0100

    Added configuration class

---
+++ src/main/java/net/gjore/jokes/config/ChuckConfiguration.java
@@ -0,0 +1,18 @@
+package net.gjore.jokes.config;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by gjorgi.hristovski on 31-Oct-18 in 11:00
+ * Created in net.gjore.jokes.config for jokes
+ */
+@Configuration
+public class ChuckConfiguration {
+
+    @Bean
+    public ChuckNorrisQuotes chuckNorrisQuotes(){
+        return new ChuckNorrisQuotes();
+    }
+}
